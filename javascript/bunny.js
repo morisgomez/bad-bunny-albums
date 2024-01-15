0b989853cde13c24f21051bd207115620769cfe3
Morís Gomez <83724852+morisgomez@users.noreply.github.com>
2024-01-14 19:25:21 -0800

    Add files via upload

---
+++ javascript/bunny.js
@@ -0,0 +1,67 @@
+//Un-Verano-Sin-Ti functions
+function revealAlbum6 () 
+{
+  document.getElementById ("Un-Verano-Sin-Ti-frame").style.display = 'block';
+  //document.getElementsByTagName ("body").style.backgroundColor = "black"; //how to make lightbox background?
+  //document.getElementsByTagName ("body").style.opacity = "0"; //how to make lightbox background?
+}
+
+function hideAlbum6 ()
+{
+  document.getElementById ("Un-Verano-Sin-Ti-frame").style.display = 'none';
+}
+
+//EL-ULTIMO-TOUR-DEL-MUNDO functions
+function revealAlbum5 () 
+{
+  document.getElementById ("EL-ULTIMO-TOUR-DEL-MUNDO-frame").style.display = 'block';
+}
+
+function hideAlbum5 ()
+{
+  document.getElementById ("EL-ULTIMO-TOUR-DEL-MUNDO-frame").style.display = 'none';
+}
+
+//LAS-QUE-NO-IBAN-A-SALIR functions
+function revealAlbum4 () 
+{
+  document.getElementById ("LAS-QUE-NO-IBAN-A-SALIR-frame").style.display = 'block';
+}
+
+function hideAlbum4 ()
+{
+  document.getElementById ("LAS-QUE-NO-IBAN-A-SALIR-frame").style.display = 'none';
+}
+
+//YHLQMDLG functions
+function revealAlbum3 () 
+{
+  document.getElementById ("YHLQMDLG-frame").style.display = 'block';
+}
+
+function hideAlbum3 ()
+{
+  document.getElementById ("YHLQMDLG-frame").style.display = 'none';
+}
+
+//OASIS functions
+function revealAlbum2 () 
+{
+  document.getElementById ("OASIS-frame").style.display = 'block';
+}
+
+function hideAlbum2 ()
+{
+  document.getElementById ("OASIS-frame").style.display = 'none';
+}
+
+//X-100PRE functions
+function revealAlbum () 
+{
+  document.getElementById ("X-100PRE-frame").style.display = 'block';
+}
+
+function hideAlbum ()
+{
+  document.getElementById ("X-100PRE-frame").style.display = 'none';
+}
